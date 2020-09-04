b6116ffd4b8a1218f063e6c0c4b1891711a4becf
Daniel Grimm <dgrimm@redhat.com>
2020-09-04 16:17:42 +0200

    OSSM ServiceMeshExtension support
    - Extend example to use a remote WASM cache
    - Deploy using ServiceMeshExtension

--- example/deploy.sh
+++ example/deploy.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
 kind create cluster || echo cluster already exists
-istioctl manifest apply -y
+
+# install maistra extension CRD
+kubectl apply -f extensions.maistra.io_servicemeshextensions.yaml
+
+istioctl manifest apply -y --set values.pilot.image=registry.gitlab.com/dgrimm/istio/pilot:wasm
+
+# patch cluster role
+kubectl apply -f patched-clusterrole.yaml
 
 kubectl create -f https://raw.githubusercontent.com/keycloak/keycloak-quickstarts/latest/kubernetes-examples/keycloak.yaml
 kubectl rollout status deployment/keycloak
 source setup-keycloak.sh
 
 kubectl label namespace default istio-injection=enabled || true
+kubectl apply -f wasm-server.yaml
+kubectl rollout status deployment/nginx-deployment
+POD=$(kubectl get pods -lapp=nginx -o jsonpath='{.items[0].metadata.name}')
+kubectl cp ../oidc.wasm  ${POD}:/var/www/oidc.wasm --container nginx
+
 kubectl apply -f httpbin.yaml
 kubectl apply -f httpbin-gateway.yaml
-
 kubectl rollout status deployment/httpbin
-HTTPBIN_POD=$(kubectl get pods -lapp=httpbin -o jsonpath='{.items[0].metadata.name}')
-kubectl cp ../oidc.wasm  ${HTTPBIN_POD}:/var/local/lib/wasm-filters/oidc.wasm --container istio-proxy
 
 kubectl apply -f istio-auth.yaml
-sed -e "s/INSERT_CLIENT_SECRET_HERE/${CLIENT_SECRET}/" envoyfilter.yaml | kubectl apply -f -
+sed -e "s/INSERT_CLIENT_SECRET_HERE/${CLIENT_SECRET}/" extension.yaml | kubectl apply -f -
